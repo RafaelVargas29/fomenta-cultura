5af67d4d86faa4c6052e5da6bd822f31a77ce9d0
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2023-05-25 15:29:04 -0300

    landing

--- www/src/pages/Home/index.tsx
+++ www/src/pages/Home/index.tsx
@@ -1,12 +1,38 @@
+import { FiLogIn } from "react-icons/fi";
+import { GoMegaphone } from "react-icons/go";
 import { Link } from "react-router-dom";
+import Images from "../../assets/brand/logo.png";
+
+import "./Home.css";
+
 export function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <div className="flex flex-col gap-12">
-        <Link to="/auth/login">Login</Link>
-        <Link to="/auth/register">Register</Link>
-        <Link to="/dashboard">Dashboard</Link>
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={Images} alt="Ecoleta" />
+        </header>
+        <main>
+          <h1>Seu marketplae de eventos culturais</h1>
+          <p>
+            Ajudamos pessoas a encontrar pontos de eventos culturais de forma
+            eficiente.
+          </p>
+          <div className="flex flex-wrap items-center lg:gap-10">
+            <Link to="/auth/login">
+              <span>
+                <FiLogIn />
+              </span>
+              <strong>Cadastrar Atividade</strong>
+            </Link>
+            <Link to="/eventos" className="alt">
+              <span>
+                <GoMegaphone />
+              </span>
+              <strong>Encontrar Atividades</strong>
+            </Link>
+          </div>
+        </main>
       </div>
     </div>
   );
--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -3,12 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: "var(--font-roboto)"
-      },
-      backgroundColor: {
-        "color-hover": ""
-      },
       screens: {
         sm: "576px",
         md: "768px",
@@ -19,11 +13,10 @@ export default {
       colors: {
         primary: "#F59E0B",
         secondary: "#8F1DDE",
-        accent: "#DE8F07",
-        neutral: "#181818",
-        "base-100": "#f0f0f5",
+        titlecolor: "#322153",
+        textcolor: "#6c6c80",
         info: "#60a5fa",
-        success: "#04D361",
+        success: "#34cb79",
         warning: "#fbbf24",
         error: "#dc2626"
       }
