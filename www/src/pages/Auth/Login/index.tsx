979eacbe874878196904c3de593400c2c418a35b
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2023-05-29 11:46:55 -0300

    feat: adiciona validação nos campos do form

--- www/src/pages/Auth/Login/index.tsx
+++ www/src/pages/Auth/Login/index.tsx
@@ -1,5 +1,41 @@
+import { ChangeEvent, useState } from "react";
 import { Link } from "react-router-dom";
+
 export function Login() {
+  const [form, setForm] = useState({
+    email: {
+      hasChanged: false,
+      value: ""
+    },
+    password: {
+      hasChanged: false,
+      value: ""
+    }
+  });
+
+  const isEmailValid = (email: string) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  function handleEmailChange(event: ChangeEvent<HTMLInputElement>) {
+    setForm({
+      ...form,
+      email: {
+        hasChanged: true,
+        value: event.target.value
+      }
+    });
+  }
+  function handlePasswordChange(event: ChangeEvent<HTMLInputElement>) {
+    setForm({
+      ...form,
+      password: {
+        hasChanged: true,
+        value: event.target.value
+      }
+    });
+  }
+
   return (
     <main
       className={`min-h-screen  flex flex-col gap-9 items-center justify-center px-4 sm:px-0`}
@@ -17,8 +53,29 @@ export function Login() {
               className="input input-clean"
               type="email"
               placeholder="E-mail"
+              value={form.email.value}
+              onChange={(event) => handleEmailChange(event)}
+              data-testid="email"
             />
-            <span className="text-xs pl-1">Digite seu e-mail </span>
+            {form.email.hasChanged && !form.email.value ? (
+              <span
+                data-testid="email-required"
+                className="text-sm font-medium leading-relaxed pl-1 text-amber-500"
+              >
+                Email é obrigatório
+              </span>
+            ) : form.email.hasChanged && !isEmailValid(form.email.value) ? (
+              <span
+                data-testid="email-invalid"
+                className="text-sm font-medium leading-relaxed pl-1 text-amber-500"
+              >
+                Email é inválido
+              </span>
+            ) : (
+              <span className="text-sm font-medium leading-relaxed pl-1">
+                Digite seu e-mail{" "}
+              </span>
+            )}
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="password">Senha</label>
@@ -27,24 +84,37 @@ export function Login() {
                 className="input input-clean"
                 type={"password"}
                 placeholder="Senha"
+                value={form.password.value}
+                onChange={(event) => handlePasswordChange(event)}
+                data-testid="password"
               />
-              <div className="cursor-pointer absolute right-3 top-2"></div>
             </div>
-            <span className="text-xs pl-1">Digite sua senha </span>
+            {form.password.hasChanged && !form.password.value ? (
+              <span
+                data-testid="password-required"
+                className="text-sm font-medium leading-relaxed pl-1 text-amber-500"
+              >
+                Senha é obrigatória
+              </span>
+            ) : (
+              <span className="text-sm font-medium leading-relaxed pl-1">
+                Digite sua senha{" "}
+              </span>
+            )}
           </div>
           <button
             type="submit"
-            className="bg-green-500 rounded font-semibold h-10 text-white hover:bg-green-600 transition-colors"
+            data-testid="login-button"
+            disabled={!isEmailValid(form.email.value) || !form.password.value}
+            className="bg-green-500 rounded font-semibold h-10 text-white hover:bg-green-600 transition-colors disabled:bg-green-500/40 disabled:text-white/40 disabled:cursor-not-allowed"
           >
             Entrar
           </button>
+
           <div className="border-t-2 border-zinc-950 mt-4 pt-4 text-sm sm:text-base">
             <p>
               Não tem uma conta?{" "}
-              <Link
-                to={"/auth/register"}
-                className="text-blue-50 hover:underline"
-              >
+              <Link to={"/auth/register"} className="text-blue-50 underline">
                 Cadastre-se
               </Link>
             </p>
--- www/src/pages/Auth/Register/index.tsx
+++ www/src/pages/Auth/Register/index.tsx
@@ -1,5 +1,52 @@
+import { ChangeEvent, useState } from "react";
 import { Link } from "react-router-dom";
 export function Register() {
+  const [form, setForm] = useState({
+    name: {
+      hasChanged: false,
+      value: ""
+    },
+    email: {
+      hasChanged: false,
+      value: ""
+    },
+    password: {
+      hasChanged: false,
+      value: ""
+    }
+  });
+  const isEmailValid = (email: string) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  function handleEmailChange(event: ChangeEvent<HTMLInputElement>) {
+    setForm({
+      ...form,
+      email: {
+        hasChanged: true,
+        value: event.target.value
+      }
+    });
+  }
+  function handlePasswordChange(event: ChangeEvent<HTMLInputElement>) {
+    setForm({
+      ...form,
+      password: {
+        hasChanged: true,
+        value: event.target.value
+      }
+    });
+  }
+  function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
+    setForm({
+      ...form,
+      name: {
+        hasChanged: true,
+        value: event.target.value
+      }
+    });
+  }
+
   return (
     <main
       className={`min-h-screen flex flex-col gap-9 items-center justify-center px-4 sm:px-0`}
@@ -19,9 +66,18 @@ export function Register() {
               placeholder="Nome da Organização"
               id="name"
               name="name"
-              value={""}
+              value={form.name.value}
+              onChange={(event) => handleNameChange(event)}
+              data-testid="name"
             />
-            <span className="text-xs pl-1">Digite seu e-mail </span>
+            {form.name.hasChanged && !form.name.value && (
+              <span
+                data-testid="name-required"
+                className="text-sm font-medium leading-relaxed pl-1 text-amber-500"
+              >
+                Nome é obrigatória
+              </span>
+            )}
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="email">E-mail</label>
@@ -29,11 +85,29 @@ export function Register() {
               className="input input-clean"
               type="email"
               placeholder="E-mail"
-              id="email"
-              name="email"
-              value={""}
+              value={form.email.value}
+              onChange={(event) => handleEmailChange(event)}
+              data-testid="email"
             />
-            <span className="text-xs pl-1">Digite seu e-mail </span>
+            {form.email.hasChanged && !form.email.value ? (
+              <span
+                data-testid="email-required"
+                className="text-sm font-medium leading-relaxed pl-1 text-amber-500"
+              >
+                Email é obrigatório
+              </span>
+            ) : form.email.hasChanged && !isEmailValid(form.email.value) ? (
+              <span
+                data-testid="email-invalid"
+                className="text-sm font-medium leading-relaxed pl-1 text-amber-500"
+              >
+                Email é inválido
+              </span>
+            ) : (
+              <span className="text-sm font-medium leading-relaxed pl-1">
+                Digite seu e-mail{" "}
+              </span>
+            )}
           </div>
           <div className="flex flex-col gap-1">
             <label htmlFor="password">Senha</label>
@@ -42,21 +116,37 @@ export function Register() {
                 className="input input-clean"
                 type={"password"}
                 placeholder="Senha"
-                id="password"
-                name="password"
-                value={""}
+                value={form.password.value}
+                onChange={(event) => handlePasswordChange(event)}
+                data-testid="password"
               />
-              <div className="cursor-pointer absolute right-3 top-2"></div>
             </div>
-            <span className="text-xs pl-1">Digite sua senha </span>
+            {form.password.hasChanged && !form.password.value ? (
+              <span
+                data-testid="password-required"
+                className="text-sm font-medium leading-relaxed pl-1 text-amber-500"
+              >
+                Senha é obrigatória
+              </span>
+            ) : (
+              <span className="text-sm font-medium leading-relaxed pl-1">
+                Digite sua senha{" "}
+              </span>
+            )}
           </div>
-
           <button
             type="submit"
-            className="bg-green-500 rounded font-semibold h-10 text-white hover:bg-green-600 transition-colors"
+            data-testid="login-button"
+            disabled={
+              !isEmailValid(form.email.value) ||
+              !form.password.value ||
+              !form.name.value
+            }
+            className="bg-green-500 rounded font-semibold h-10 text-white hover:bg-green-600 transition-colors disabled:bg-green-500/40 disabled:text-white/40 disabled:cursor-not-allowed"
           >
             Inscrever-se
           </button>
+
           <div className="border-t-2 border-zinc-950 mt-4 pt-4 text-sm sm:text-base">
             <p>
               Você já tem conta?{" "}
