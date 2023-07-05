d26adf4b88dfc2b40cee08df97976ddc2626dec5
alexsandro-cristiano <acg.alexsandro@homtial.com>
2023-07-05 17:17:46 -0300

    end no details activiti

--- www/src/pages/Details/index.tsx
+++ www/src/pages/Details/index.tsx
@@ -1,14 +1,15 @@
+import { useEffect, useState } from "react";
+import { BiLeftArrowAlt } from "react-icons/bi";
+import { useNavigate, useParams } from "react-router-dom";
+import { useContextSelector } from "use-context-selector";
 import { Logo } from "../../components/Logo";
-import Wrapper from "../../templates/Wrapper";
 import { Activities } from "../../model/Activities";
 import { ActivitiesContext } from "../../store/context/ActivitiesContext";
-import { useContextSelector } from "use-context-selector";
-import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { BiLeftArrowAlt } from "react-icons/bi";
+import Wrapper from "../../templates/Wrapper";
+import { forma } from "../../utils/formatter";
 
 export default function Details() {
-  const { id } = useParams(); 
+  const { id } = useParams();
   const navigate = useNavigate();
   const [activity, setActivity] = useState<Activities | null>(null);
   const { activities } = useContextSelector(ActivitiesContext, (context) => {
@@ -35,11 +36,11 @@ export default function Details() {
 
       <main className="mt-32">
         <Wrapper>
-            <span
+          <span
             className=" text-primary/60 flex items-center font-bold mt-3 ml-4 cursor-pointer hover:text-primary"
             onClick={() => navigate(-1)}
             title="voltar"
-            >
+          >
             <BiLeftArrowAlt className="icon w-8 h-8" />
           </span>
         </Wrapper>
@@ -48,30 +49,43 @@ export default function Details() {
             <div className="flex w-full h-[550px] rounded-lg overflow-hidden shadow-md">
               <div className="w-1/2">
                 {activity && (
-                  <img src={activity.image} alt="" className="h-full object-cover" />
+                  <img
+                    src={activity.image}
+                    alt=""
+                    className="h-full object-cover"
+                  />
                 )}
               </div>
               <div className="w-1/2 bg-secondary p-6 flex flex-col justify-center">
                 {activity && (
                   <div className="text-white">
-                    <h1 className="text-6xl font-bold leading-tight mb-6">{activity.title}</h1>
+                    <h1 className="text-6xl font-bold leading-tight mb-6">
+                      {activity.title}
+                    </h1>
                     <div className="text-2xl mb-2">
-                      <span className="font-bold">Descrição:</span> {activity.description}
+                      <span className="font-bold">Descrição:</span>{" "}
+                      {activity.description}
                     </div>
                     <div className="text-2xl mb-2">
-                      <span className="font-bold">Categoria:</span> {activity.category}
+                      <span className="font-bold">Categoria:</span>{" "}
+                      {activity.category}
                     </div>
                     <div className="text-2xl mb-2">
-                      <span className="font-bold">Status:</span> {activity.status}
+                      <span className="font-bold">Status:</span>{" "}
+                      {activity.status}
                     </div>
                     <div className="text-2xl">
-                      <span className="font-bold">Data:</span> {activity.dateEvent}
+                      <span className="font-bold">Data:</span>{" "}
+                      {forma(activity.dateEvent)}
                     </div>
                     <div className="text-2xl">
-                      <span className="font-bold">Horário:</span> {activity.hoursEvent}
+                      <span className="font-bold">Horário:</span>{" "}
+                      {activity.hoursEvent}
                     </div>
                     <div className="text-2xl mb-2">
-                      <span className="font-bold">Endereço:</span> {activity.address?.localidade}
+                      <span className="font-bold">Endereço:</span>{" "}
+                      {activity.address?.logradouro}{" "}
+                      {activity.address?.localidade}
                     </div>
                   </div>
                 )}
