1ddce2eb93ae5189c185b8912f93d6731bc8eed3
alexsandro-cristiano <acg.alexsandro@homtial.com>
2023-07-09 21:42:06 -0300

    bd apresentacao

--- www/src/lib/firebase/config/index.ts
+++ www/src/lib/firebase/config/index.ts
@@ -1,24 +1,31 @@
 import { initializeApp } from "firebase/app";
 
+//  const firebaseConfig = {
+//  apiKey: "AIzaSyBBKyqEatcxxzguf7XEzW5XN4FGQFoiB_E",
+//  authDomain: "tcc-fomenta-cultura.firebaseapp.com",
+//  projectId: "tcc-fomenta-cultura",
+//  storageBucket: "tcc-fomenta-cultura.appspot.com",
+//  messagingSenderId: "636194722165",
+//  appId: "1:636194722165:web:52e81b08f04055e167833c"
+//  };
+
 // const firebaseConfig = {
-//   apiKey: "AIzaSyBBKyqEatcxxzguf7XEzW5XN4FGQFoiB_E",
-//   authDomain: "tcc-fomenta-cultura.firebaseapp.com",
-//   projectId: "tcc-fomenta-cultura",
-//   storageBucket: "tcc-fomenta-cultura.appspot.com",
-//   messagingSenderId: "636194722165",
-//   appId: "1:636194722165:web:52e81b08f04055e167833c"
+//   apiKey: "AIzaSyD38kUhwCE-3KzP32sObwNul5de_kpPiRs",
+//   authDomain: "teste-firebase-1a95c.firebaseapp.com",
+//   projectId: "teste-firebase-1a95c",
+//   storageBucket: "teste-firebase-1a95c.appspot.com",
+//   messagingSenderId: "876163684607",
+//   appId: "1:876163684607:web:a363de19806e378d5094ca",
+//   measurementId: "G-403LYYM1YT"
 // };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD38kUhwCE-3KzP32sObwNul5de_kpPiRs",
-  authDomain: "teste-firebase-1a95c.firebaseapp.com",
-  projectId: "teste-firebase-1a95c",
-  storageBucket: "teste-firebase-1a95c.appspot.com",
-  messagingSenderId: "876163684607",
-  appId: "1:876163684607:web:a363de19806e378d5094ca",
-  measurementId: "G-403LYYM1YT"
+  apiKey: "AIzaSyAsDMmedoUqJ-RVj6_y7_0Eipf7nqwNQM8",
+  authDomain: "apresentacao-tcc-verdade.firebaseapp.com",
+  projectId: "apresentacao-tcc-verdade",
+  storageBucket: "apresentacao-tcc-verdade.appspot.com",
+  messagingSenderId: "399182445421",
+  appId: "1:399182445421:web:ff0e9316f4ca7125873864"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export { app };
